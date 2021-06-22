2f36559ef64cfa92090f4643863621efb6f33f0e
Kenny <50640060+Feierstein1@users.noreply.github.com>
2021-06-21 20:54:41 -0400

    Update
    bigger display,
    display tie game,
    display winner scores,
    highlight winner row and reset

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 let currentPlayer = "X" 
+let playerXscore = 0
+let playerOscore = 0
+let tieGame = false
 let turnCount = 0
 let isWinner = false
 let isComputer = false
@@ -33,12 +36,16 @@ const MoveClick =(id)=>{
         if(turnCount > 4){
             isWinner = isWinningPlay()
         }
+
         if(!isWinner){
             currentPlayer = switchPlayer()
             setDisplay()
         }
         else{
             setPlayerDisplay(`Player ${currentPlayer} WON!!!!`)
+        }
+        if(!isWinner && turnCount > 9){
+            setPlayerDisplay(`TIE GAME!!!!`)
         }
     }
     if(isComputer && currentPlayer == "O"){
@@ -51,6 +58,7 @@ const resetBoard =()=>{
     let optionBoxes = document.querySelectorAll(".option-box")
     for(let opt of optionBoxes){
         opt.innerHTML = ""
+        document.getElementById(opt.id).classList.remove("highlight")
     }
     resetDisplay()
 }
@@ -60,20 +68,28 @@ const setPlayerDisplay =(msg)=>{
 }
 
 const incrementTurnCountDisplay =()=>{
-    turnCount < 10 ? turnCount++ : turnCount    
-    document.getElementById("turnCount").innerHTML = `round: ${turnCount}`
+    turnCount <= 9 ? turnCount++ : turnCount    
+    document.getElementById("turnCount").innerHTML = `Round: ${turnCount}`
 }
 
 const resetDisplay =()=>{
-    turnCount = 0
-    currentPlayer = "X" 
-    isWinner = false
-    setDisplay()
+    if(turnCount != 0){
+        turnCount = 0
+        currentPlayer = "X" 
+        isWinner = false
+        setDisplay()
+    }
 }
 
 const setDisplay =()=>{
     incrementTurnCountDisplay()
     setPlayerDisplay(`Player ${currentPlayer}'s turn`)
+    setScores()
+}
+
+const setScores =()=>{
+    document.getElementById("X_score").innerHTML = `Player X Score: ${playerXscore}`
+    document.getElementById("O_score").innerHTML = `Player O Score: ${playerOscore}`
 }
 
 const isWinningPlay =()=>{
@@ -82,6 +98,11 @@ const isWinningPlay =()=>{
         let setArray = document.querySelectorAll(`.${set}`)
         if((setArray[0].innerHTML === setArray[1].innerHTML && setArray[1].innerHTML === setArray[2].innerHTML) && setArray[0].innerHTML.length){
             console.log(`${set} ${setArray[0].innerHTML}, ${setArray[1].innerHTML}, ${setArray[2].innerHTML}`)
+            currentPlayer === "X" ? playerXscore++ : playerOscore++
+            setScores()
+            for (i of setArray){
+                document.getElementById(i.id).classList.add("highlight")
+            }
             return true
         }
     }
